c7e617a4b5da25f5462bc5944d926269fec812a8
ch4t5ky <mr_a953@mail.ru>
2021-08-28 17:32:42 +0300

    #3: Create pagination for parsing all values from query

--- scrapper/src/start.py
+++ scrapper/src/start.py
@@ -3,5 +3,4 @@ from yandex_patents.robot import Searcher
 
 if __name__ == '__main__':
     search = Searcher()
-    search.find_information_by_keyword("pizza")
-    # print(search.get_info_from_url("https://www1.fips.ru/iiss/document.xhtml?faces-redirect=true&id=aa0c8c9dec090a7ccc00eb61e55e2165"))
+    print(search.find_information_by_keyword("пицца"))
--- scrapper/src/yandex_patents/robot.py
+++ scrapper/src/yandex_patents/robot.py
@@ -11,6 +11,8 @@ class Searcher:
         "Host": "yandex.com",
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 YaBrowser/21.6.4.786 Yowser/2.5 Safari/537.36"
     }
+    SLEEP_BETWEEN_SIMPLE_REQUEST = 5
+    DOCS_PER_REQUEST = 50
 
     def __init__(self):
         self._session = requests.Session()
@@ -21,25 +23,41 @@ class Searcher:
         headers = {
             'Referer': f"https://yandex.com/patents?dco=RU&dco=SU&dl=ru&dt=0&dty=1&dty=2&s=0&sp=0&spp=10&st=0&{urlencode({'text': keyword})}"
         }
+        result = []
+        p = 0
+        url = self.create_url_with_payload(keyword, p)
+        json_content = self._session.get(url, headers=headers).json()
+        doc_amount = json_content['TotalDocCount']
+        max_page = doc_amount[0] // self.DOCS_PER_REQUEST
+        while p < max_page:
+            url = self.create_url_with_payload(keyword, p)
+            resp = self._session.get(url, headers=headers)
+            if resp.status_code != requests.codes.OK:
+                break
+            json_content = resp.json()
+            pattents = self.convert_patent_to_format(json_content['Grouping'][0])
+            print(len(pattents))
+            result.extend(pattents)
+            p += 1
+        return result
+
+    def create_url_with_payload(self, keyword, p) -> str:
         payload = {
             "text": keyword,
             "template": "%request% << (s_19_country:RU) << (i_doc_type:1 | i_doc_type:2)",
-            "p": 0,
+            "p": p,
             "how": "rlv",
-            "numdoc": 10
+            "numdoc": 50
         }
-        url = f"{self._URL}/api/search?{urlencode(payload)}"
-        json_content = self._session.get(url, headers=headers).json()
-        return self.convert_patent_to_format(json_content['Grouping'][0])
 
-    def get_patent_by_doc_id(self, doc_id: str):
-        pass
+        return f"{self._URL}/api/search?{urlencode(payload)}"
 
     @staticmethod
     def convert_patent_to_format(groups: dict) -> list:
         result = []
         for group in groups["Group"]:
-            attributes = group['Document'][0]['ArchiveInfo']['GtaRelatedAttribute']
+            archive = group['Document'][0]['ArchiveInfo']
+            attributes = archive['GtaRelatedAttribute']
             tags = {d['Key']: d['Value'] for d in attributes}
             patent = PatentInfo(
                 title=tags.get('z_ru_54_name', ''),
